857a429fc2715f75dc4fa7e2639da03ec25e2302
Max <gusstorm132@gmail.com>
2018-06-26 09:33:19 -0700

    skeletal mvc framework

---
+++ StudentEnrollment/StudentEnrollment/Data/EnrollmentDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentEnrollment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Data
+{
+    public class EnrollmentDbContext : DbContext
+    {
+        public EnrollmentDbContext(DbContextOptions<EnrollmentDbContext> options ) : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ StudentEnrollment/StudentEnrollment/Models/Course.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int MaxStudents { get; set; }
+    }
+
+    public enum Enum
+    {
+        Lying101 = 1,
+        ArtOfTheGrift,
+        WasteOfTime,
+        FreeDegree605
+    }
+}
